78731351b93c32d53e4718091342e97d9d555441
tajkiatoma <tajkiatoma@gmail.com>
2025-07-11 12:20:01 -0600

    Fix: add placeholders in constants file

--- util/constants.py
+++ util/constants.py
@@ -1,1 +1,7 @@
 TOP_N = 1000
+
+GEMINI_2_FLASH_THINKING = 'gemini-2.0-flash-thinking-exp-01-21' # VertexAI
+GEMINI_API_KEY = ''
+
+GPT_4O_MINI = 'gpt-4o-mini-2024-07-18' # OpenAI
+OPENAI_API_KEY = ''
